b5abb9fce5da3e75df8437461be3ebea0d7174e6
Ozzy <snekshaark@gmail.com>
2018-05-19 23:50:31 +0100

    Start messing with colours

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import styled from 'react-emotion'
 import Link from 'gatsby-link'
 
 import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
+const Title = styled.h2`
+  color: #CCD9CE;
+  font-size: 1.9em;
+  height: 1.9rem;
+  line-height: 1.6;
+`
+
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+        <Link to="/" lassName="navbar-item">
+          <Title>ornous.com</Title>
         </Link>
       </div>
       <div className="navbar-start">
