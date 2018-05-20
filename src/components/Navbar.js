2abc8478d906b6adc33fa138ce3647262346a701
Ozzy <snekshaark@gmail.com>
2018-05-20 17:54:51 +0100

    Add images to blog posts + update github url

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => (
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/ornous/blog"
           target="_blank"
           rel="noopener noreferrer"
         >
